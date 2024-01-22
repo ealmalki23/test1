70381e8f2f115eb261fbd7185b086bfee30287b9
ealmalki23 <147074692+ealmalki23@users.noreply.github.com>
2024-01-22 17:43:12 +0300

    Initial Commit

---
+++ test1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test1
+//
+//  Created by Eman Almalki  on 10/07/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
